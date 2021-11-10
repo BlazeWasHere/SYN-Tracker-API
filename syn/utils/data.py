29a97a3ae6b83355b251b4d0d87085db25f5d8af
BlazeWasHere <blaze@dorime.org>
2021-11-10 18:41:06 +0000

    no need to host data for defillama

--- syn/utils/data.py
+++ syn/utils/data.py
@@ -187,59 +187,6 @@ for key, value in SYN_DATA.items():
                             abi=BASEPOOL_ABI)  # type: ignore
         })
 
-# Data for the adaper: https://github.com/DefiLlama/DefiLlama-Adapters/blob/main/projects/synapse/index.js
-DEFILLAMA_DATA = {
-    "bridges": {
-        "bsc": {
-            "metaswap": "0x930d001b7efb225613ac7f35911c52ac9e111fa9",
-            "usd-lp": "0xf0b8b631145d393a767b4387d08aa09969b2dfed",
-            "obscure": "0x14016e85a25aeb13065688cafb43044c2ef86784",
-            "obscure-decimals": 18
-        },
-        "ethereum": {
-            "metaswap": "0x2796317b0fF8538F253012862c06787Adfb8cEb6",
-            "obscure": "0x8e870d67f660d95d5be530380d0ec0bd388289e1",
-            "obscure-decimals": 18
-        },
-        "polygon": {
-            "metaswap": "0x96cf323E477Ec1E17A4197Bdcc6f72Bb2502756a",
-            "usd-lp": "0x128a587555d1148766ef4327172129b50ec66e5d",
-            "obscure": "0x104592a158490a9228070e0a8e5343b499e125d0",
-            "obscure-decimals": 18
-        },
-        "avax": {
-            "metaswap": "0xf44938b0125a6662f9536281ad2cd6c499f22004",
-            "usd-lp": "0x55904f416586b5140a0f666cf5acf320adf64846",
-            "obscure": "0x4fbf0429599460D327BD5F55625E30E4fC066095",
-            "obscure-decimals": 18
-        },
-        "fantom": {
-            "metaswap": "0xaed5b25be1c3163c907a471082640450f928ddfe",
-            "obscure": "0x04068da6c83afcfa0e13ba15a6696662335d5b75",
-            "obscure-decimals": 6
-        },
-        "arbitrum": {
-            "obscure": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
-            "obscure-decimals": 18
-        }
-    },
-    "subgraphs": {
-        "bsc":
-        "https://api.thegraph.com/subgraphs/name/aureliusbtc/bsc-synapse-amm",
-        "ethereum":
-        "https://api.thegraph.com/subgraphs/name/aureliusbtc/mainnet-synapse-amm",
-        "polygon":
-        "https://api.thegraph.com/subgraphs/name/aureliusbtc/polygon-synapse-amm",
-        "avax":
-        "https://api.thegraph.com/subgraphs/name/aureliusbtc/avalanche-synapse-amm",
-        "arbitrum":
-        "https://api.thegraph.com/subgraphs/name/aureliusbtc/arbitrum-synapse-amm",
-        "fantom":
-        "https://api.thegraph.com/subgraphs/name/aureliusbtc/fantom-synapse-amm"
-    },
-    "unsupported": ["nUSD", "Frapped USDT", "Magic Internet Money", "nETH"]
-}
-
 TOKEN_DECIMALS = {
     'eth': {
         '0x71ab77b7dbb4fa7e017bc15090b2163221420282': 18,
