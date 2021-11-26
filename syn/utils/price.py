427bf84a36dc4c3a4aa8d7045a304cae3ca23a87
BlazeWasHere <blaze@dorime.org>
2021-11-26 18:33:38 +0000

    okay this time i really fixed the price bug

--- syn/utils/price.py
+++ syn/utils/price.py
@@ -216,7 +216,7 @@ def get_historic_price_syn(date: str, currency: str = "usd") -> Decimal:
 def get_historic_price_for_address(chain: str, address: str,
                                    date: str) -> Decimal:
     if address in CUSTOM[chain]:
-        return CUSTOM[chain][address]
+        return Decimal(CUSTOM[chain][address])
     elif ADDRESS_TO_CGID[chain][address] == CoingeckoIDS.SYN:
         return get_historic_price_syn(date)
 
