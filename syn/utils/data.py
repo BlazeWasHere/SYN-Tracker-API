9213420e81ddfe1a058e5bed5d0a7beb04871e51
Trajan0x <trajan0x@users.noreply.github.com>
2022-06-17 17:50:12 -0400

    add more redis data

--- syn/utils/data.py
+++ syn/utils/data.py
@@ -70,11 +70,15 @@ else:
 # We use this for processes to interact w/ eachother.
 MESSAGE_QUEUE_REDIS_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/3'
 MESSAGE_QUEUE_REDIS = redis.Redis.from_url(MESSAGE_QUEUE_REDIS_URL,
+                                           password=os.environ['REDIS_PASSWORD'],
+                                           username=os.environ['REDIS_USERNAME'],
                                            decode_responses=True)
 # We use this for storing eth_GetLogs and stuff related to that.
 LOGS_REDIS_URL = redis.Redis(REDIS_HOST,
                              REDIS_PORT,
                              db=4,
+                             password=os.environ['REDIS_PASSWORD'],
+                             username=os.environ['REDIS_USERNAME'],
                              decode_responses=True)
 
 _POPULATE_CACHE = os.getenv('POPULATE_CACHE')
